82a64c7260cacc73c3216b25e2a96e30ce095729
franciskone <francesco.cappelli.developer@gmail.com>
2019-09-14 23:24:52 +0100

    wip on ItineraryItem

---
+++ client/src/components/SearchResults/ItineraryItem/ItineraryItem.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const ItineraryItem = () => (
+  <div>
+    ItineraryItem
+  </div>);
+
+// ItineraryItem.propTypes = {
+//   flights: PropTypes.arrayOf({
+//     carrier: PropTypes.string,
+//   }).isRequired,
+// };
+// ItineraryItem.defaultProps = {};
+
+export default ItineraryItem;
+
---
+++ client/src/components/SearchResults/SearchResultsActions/SearchResultsActions.test.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TestRenderer from 'react-test-renderer';
+
+import SearchResultsActions from './SearchResultsActions';
+
+describe('SearchResultsActions', () => {
+  it('should render correctly', () => {
+    const tree = TestRenderer.create(<SearchResultsActions />).toJSON();
+    
+    expect(tree).toMatchSnapshot();
+  });
+});
+
